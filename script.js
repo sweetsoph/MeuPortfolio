b4106d829cc455aae0ef0af8e8e545ed672a7b71
Sophia Verardo <101429997+sweetsoph@users.noreply.github.com>
2022-05-20 10:44:45 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+<script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.12"></script>
+        <script>
+            var typed = new Typed(".input", {
+                strings: ["Felipe Barbosa.", "Programador.", "Full Stack." , "Web Designer."],
+                typeSpeed: 70,
+                backSpeed: 60,
+                loop: true
+            });
+        </script>
+    </body>
